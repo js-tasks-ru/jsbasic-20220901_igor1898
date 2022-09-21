c38af1b304728295d1e8b14e1542ba6df4eb9c8c
Igor1D <43789550+Igor1D@users.noreply.github.com>
2022-09-21 21:04:31 +0200

    Module 1 task 3

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -1,3 +1,8 @@
-function ucFirst(str) {
-  // ваш код...
+function ucFirst(str = '') {
+  if (str != '') {
+    return str.charAt(0).toUpperCase([0]) + str.slice(1);
+  } else {
+    return '';
+  }
 }
+ucFirst('Igor');
